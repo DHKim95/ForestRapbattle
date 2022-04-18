212e72232599e9b1f22e2025f19a2c222e7e0566
sunghyun <shcho2112@naver.com>
2022-04-19 00:14:53 +0900

    S06P31E204-46 feature react 환경 구축

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Nav.js'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
