3290312cf12ea6fdd884f897b7b743daa87c5f2c
xogxog <nasa0939@naver.com>
2022-04-27 11:40:04 +0900

    S06P31E204-94 feature DB 테이블 구축

--- server/forestrapbattle/urls.py
+++ server/forestrapbattle/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('accounts.urls')), # 홈페이지 전반
+    path('api/v1/game/', include('game.urls')), # 게임 화면 용
 ]
