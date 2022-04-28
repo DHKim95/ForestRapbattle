1ece870bec5be67e52e05e9dae0bdc122c9d82b8
sunghyun <shcho2112@naver.com>
2022-04-29 04:55:28 +0900

    S06P31E204-49 feature 메인페이지

---
+++ Frontend/src/components/Navbar.tsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import "../styles/Navbar.scss";
+import { alpha, styled } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { Link } from "react-router-dom";
+import myImg from "../assests/sunghyun.jpg";
+
+const pages = ["게임시작", "로그인", "Sunghyun"];
+
+const RootStyle = styled(AppBar)(({ theme }) => ({
+  boxShadow: "none",
+  backdropFilter: "blur(6px)",
+  WebkitBackdropFilter: "blur(6px)",
+  backgroundColor: alpha(grey[900], 0.72),
+  [theme.breakpoints.down("sm")]: {},
+}));
+
+const ToolbarStyle = styled(Toolbar)({
+  height: 80,
+  padding: "0px !important",
+  letterSpacing: "0px",
+  width: "100%",
+});
+
+const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <RootStyle>
+      <Container maxWidth="xl">
+        <ToolbarStyle disableGutters>
+          <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" }, flexGrow: 1 }} className="aaa">
+            <Link to="/">Forest Rap Battle</Link>
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            Logo
+          </Typography>
+          <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: "white", display: "block" }}>
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp">
+                  <img src={myImg} alt="dd" style={{ width: "100%" }} />
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </ToolbarStyle>
+      </Container>
+    </RootStyle>
+  );
+};
+export default Navbar;
