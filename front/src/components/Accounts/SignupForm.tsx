5ef119f171a37248fd74376d92a0d8c704ee9a51
Sungtong1 <shcho2112@naver.com>
2022-05-08 06:39:06 +0900

    S06P31E204-120 feature 로그인 회원가입 로그아웃 백엔드와 연동

---
+++ front/src/components/Accounts/SignupForm.tsx
@@ -0,0 +1,330 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import TextFieldWithButton from "./TextFieldWithButton";
+import { Button, Container, TextField, Typography } from "@mui/material";
+// import { makeStyles } from "@mui/styles";
+import { useNavigate } from "react-router-dom";
+import { SignupUserInfo } from "../../types/account";
+import { customAxios} from "../../customAxios";
+import { errorControl, loadingControl } from "../../redux/baseInfo/actions";
+import { connect } from "react-redux";
+import { userLogin } from "../../redux/account/actions";
+import { LoginUserInfo } from "../../types/account";
+import { AccountReducer } from "../../redux/rootReducer";
+
+
+function SignupForm({ errorControl, loadingControl,userLogin, isLogin }: Props) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLogin) {
+      navigate("/");
+    }
+  }, [isLogin, navigate]);
+  // 유저정보 기본값
+  const initUserInfo: SignupUserInfo = {
+    username: "",
+    password: "",
+    passwordConfirmation: "",
+    nickname: "",
+    agreement: false,
+    profileId: 1,
+  };
+
+  // 유저정보 state
+  const [userInfo, setUserInfo] = useState<SignupUserInfo>(initUserInfo);
+
+  // 인증관련 state
+  const [nicknameConfirmation, setNicknameConfirmation] = useState<boolean>(false);
+  const [sendCheckNickname, setSendCheckNickname] = useState<boolean>(false);
+
+  // 유효성 검사 처리
+  const [emailMessage, setEmailMessage] = useState<string>("");
+  const [nickNameMessage, setNickNameMessage] = useState<string>("");
+  const [passwordMessage, setPasswordMessage] = useState<string>("");
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState<string>("");
+
+  // profile 페이지
+  const [page, setPage] = useState<number>(1);
+
+  // 프로필 이미지
+  type ProfileImage = {
+    profile_id: number;
+    profile_img: string;
+  };
+  const [profileImages, setProfileImages] = useState<ProfileImage[]>([{ profile_id: 0, profile_img: "" }]);
+  function ProfileSelector() {
+    function Selected(ccc: number): void {
+      const newUserInfo: SignupUserInfo = { ...userInfo };
+      newUserInfo["profileId"] = ccc;
+      setUserInfo(() => newUserInfo);
+    }
+    const aaa = [];
+    for (let index = 0; index < profileImages.length; index++) {
+      let bbb = profileImages[index];
+      aaa.push(
+        <div key={bbb.profile_id}>
+          <img
+            src={bbb.profile_img}
+            alt="profile"
+            onClick={(event) => {
+              Selected(bbb.profile_id);
+            }}
+            style={{ width: "50px" }}
+          ></img>
+        </div>
+      );
+    }
+    return <div>{aaa}</div>;
+  }
+  useEffect(() => {
+    const getProfileImage = async () => {
+      const profileRes = await customAxios({
+        method: "get",
+        url: `${process.env.REACT_APP_BASE_URL}/api/v1/auth/profileImages`,
+      });
+      console.log(profileRes.data);
+      setProfileImages(profileRes.data);
+    };
+    getProfileImage();
+  }, []);
+
+  // 회원가입 요청전송
+  const requestSignup = async () => {
+    loadingControl(true);
+
+    try {
+      const res = await customAxios({
+        method: "post",
+        url: `${process.env.REACT_APP_BASE_URL}/api/v1/auth/signup`,
+        data: {
+          email: userInfo.username,
+          password: userInfo.password,
+          password2: userInfo.passwordConfirmation,
+          nickname: userInfo.nickname,
+          profile_id: userInfo.profileId,
+        },
+      });
+      if (res.status !== 200) {
+        throw new Error("SinupFailed");
+      }
+    } catch (e: any) {
+      if (e.message === "SinupFailed") {
+        errorControl("회원가입에 실패했습니다.");
+      } else {
+        errorControl("알수없는 에러가 발생했습니다.");
+      }
+    }
+    const loginData: LoginUserInfo = {
+      username: userInfo.username,
+      password: userInfo.password,
+    };
+    userLogin(loginData);
+    console.log(loginData);
+    console.log("성공");
+    loadingControl(false);
+  };
+  // page 이동 함수
+  function goToProfile(): void {
+    if (page === 1) {
+      setPage(2);
+    }
+  }
+
+  // 닉네임 중복검사요청
+  function requsetCheckNickname(): void {
+    // 닉네임 재작성
+    if (sendCheckNickname) {
+      setSendCheckNickname(() => false);
+      setNicknameConfirmation(() => false);
+    } else {
+      // 닉네임 인증
+      setSendCheckNickname(() => true);
+      axios({
+        method: "post",
+        url: `${process.env.REACT_APP_BASE_URL}/api/v1/auth/nickname`,
+        data: { nickname: userInfo.nickname },
+      })
+        .then((res) => {
+          setNicknameConfirmation(() => true);
+        })
+        .catch((err) => {
+          setSendCheckNickname(() => false);
+        });
+    }
+  }
+
+  // 회원가입폼 정보입력
+  function changeUserInfo(event: React.ChangeEvent<HTMLInputElement>): void {
+    const newUserInfo: SignupUserInfo = { ...userInfo };
+    const targetId: string = event.target.id;
+    newUserInfo[targetId] = event.target.value;
+    setUserInfo(() => newUserInfo);
+    // 유효성 검사
+    switch (targetId) {
+      case "username":
+        const emailRegex = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        if (!newUserInfo.username || emailRegex.test(newUserInfo.username)) {
+          setEmailMessage("");
+        } else {
+          setEmailMessage("이메일 형식이 틀렸습니다.");
+        }
+        break;
+      case "password":
+        // 비밀번호 유효성 검사
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+        if (!newUserInfo.password || passwordRegex.test(newUserInfo.password)) {
+          setPasswordMessage("");
+        } else {
+          setPasswordMessage("숫자+영문자+특수문자 조합으로 8자리이상 입력하세요.");
+        }
+        // 비밀번호 확인 일치검사
+        if (!newUserInfo.passwordConfirmation || newUserInfo.password === newUserInfo.passwordConfirmation) {
+          setPasswordConfirmMessage("");
+        } else {
+          setPasswordConfirmMessage("비밀번호가 일치하지 않습니다.");
+        }
+
+        break;
+      case "passwordConfirmation":
+        if (!newUserInfo.passwordConfirmation || newUserInfo.password === newUserInfo.passwordConfirmation) {
+          setPasswordConfirmMessage("");
+        } else {
+          setPasswordConfirmMessage("비밀번호가 일치하지 않습니다.");
+        }
+        break;
+      case "nickname":
+        if (newUserInfo.nickname.length < 2 || newUserInfo.nickname.length > 8) {
+          setNickNameMessage("닉네임은 2글자 이상 8글자 이하로 입력해 주세요");
+        } else {
+          setNickNameMessage("");
+        }
+
+        break;
+
+      default:
+        return;
+    }
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        position: "absolute",
+        alignItems: "center",
+        height: "calc(100% - 80px)",
+        width: "100%",
+      }}
+    >
+      {page === 2 || (
+        <Container component="main" maxWidth="sm">
+          <div>
+            <Typography component="h1" variant="h5">
+              회원가입
+            </Typography>
+            <form noValidate>
+              <TextField label="이메일" id="username" autoComplete="email" fullWidth onChange={changeUserInfo} helperText={emailMessage}></TextField>
+              <TextField
+                error={!!passwordMessage}
+                helperText={passwordMessage}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="password"
+                label="비밀번호"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={changeUserInfo}
+              />
+              <TextField
+                error={!!passwordConfirmMessage}
+                helperText={passwordConfirmMessage}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="passwordConfirmation"
+                label="비밀번호 확인"
+                type="password"
+                id="passwordConfirmation"
+                onChange={changeUserInfo}
+              />
+              <TextFieldWithButton
+                label="닉네임"
+                id="nickname"
+                onChange={changeUserInfo}
+                onClickButton={requsetCheckNickname}
+                buttonText="닉네임중복확인"
+                disabled={sendCheckNickname}
+                helperText={nickNameMessage}
+              ></TextFieldWithButton>
+
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={goToProfile}
+                disabled={
+                  userInfo.password !== userInfo.passwordConfirmation ||
+                  userInfo.username === "" ||
+                  userInfo.password === "" ||
+                  userInfo.passwordConfirmation === "" ||
+                  userInfo.nickname === "" ||
+                  nicknameConfirmation === false
+                }
+              >
+                회원가입
+              </Button>
+            </form>
+          </div>
+        </Container>
+      )}
+      {page === 1 || (
+        <Container component="main" maxWidth="sm">
+          <Typography component="h1" variant="h5">
+            프로필 선택
+          </Typography>
+          <ProfileSelector></ProfileSelector>
+          <div>{userInfo.profileId}</div>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={requestSignup}
+            disabled={
+              userInfo.password !== userInfo.passwordConfirmation ||
+              userInfo.username === "" ||
+              userInfo.password === "" ||
+              userInfo.passwordConfirmation === "" ||
+              userInfo.nickname === "" ||
+              nicknameConfirmation === false
+            }
+          >
+            회원가입
+          </Button>
+        </Container>
+      )}
+    </div>
+  );
+}
+
+const mapStateToProps = ({ account }: AccountReducer) => {
+  return {
+    isLogin: account.isLogin,
+  };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    errorControl: (errorMessage: string) => {
+      errorControl(dispatch, errorMessage);
+    },
+    loadingControl: (state: boolean) => {
+      loadingControl(dispatch, state);
+    },
+    userLogin: (userInfo: LoginUserInfo) => dispatch(userLogin(userInfo)),
+  };
+};
+type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import store from "./redux/store"
+
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+//   );
+// const persistor = persistStore(store);
+// root.render(
+//   <Provider store={store}>
+//       <PersistGate persistor={persistor}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+// );
+const persistor = persistStore(store);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
